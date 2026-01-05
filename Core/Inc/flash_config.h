fc51e4120e54ef7f379f5edcf32ce8aaf64f1b75
rturca <radomir.turca@codelec.cz>
2026-01-05 10:00:44 +0100

    flash

--- Core/Inc/flash_config.h
+++ Core/Inc/flash_config.h
@@ -3,8 +3,8 @@
 #define FLASH_BOOTLDR_PAYLOAD_SIZE_KB 	120
 #define FLASH_BOOTLDR_SIZE_KB 			8
 
-#define USB_VID 	0x0483
-#define USB_PID		0xDF11
+#define USB_VID 						0x0483
+#define USB_PID							0xDF11
 
 #define WINUSB_SUPPORT					1
 #define ENABLE_DFU_UPLOAD				1
